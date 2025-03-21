f8effe5ef52f7d071225185543bc368f3c6b62bf
merkis88 <axuenno88@gmail.com>
2025-03-21 13:37:00 +0700

    column

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+Vue.component('column', {
+    props: ['column', 'index'],
+    template: `
+        <div class="column">
+            <h2>{{ column.title }}</h2>
+            <button v-if="index === 0" @click="addCard">Добавить задачу</button>
+        </div>
+    `,
+    methods: {
+        addCard() {
+            const newCard = {
+                id: Date.now(),
+                title: '',
+                description: '',
+                createdAt: new Date().toLocaleString(),
+                deadline: null,
+                lastEdited: null,
+                returnReason: null,
+                status: null
+            };
+            this.column.cards.push(newCard);
+        }
+    }
+});
+
+
 const app = new Vue ({
     el: '#app',
     data: {
@@ -9,3 +35,4 @@ const app = new Vue ({
         ]
     }
 });
+
